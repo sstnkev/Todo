a0c981083b39e1b85f1221d3ec4d0792c0f7eabd
sstnkev <stankewitch.nikita@gmail.com>
2024-03-27 21:18:22 +0300

    sstnkev-todo

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const addtext = document.getElementById('control-add') // кнопка add
+// add.addEventListener('click', function(){
+    
+// })
+const deleteAll = document.getElementById('control-delete')  // кнопка delete
+const enterText = document.getElementById('control-enter')  // поле с текстом
+const completeTodo = document.getElementById('todo-complete')  // кнопка галочка, которая убирает лист с зеленым цветом экрана
+const closeTodo = document.getElementById('todo-close')  // кнопка крестик, которая убирает лист с красным цветом экрана
